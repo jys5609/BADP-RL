d147695d290b0d7b6afde1750fe3e6f330ad0856
Jongmin Lee <starjongmin@gmail.com>
2019-08-27 04:35:08 +0000

    condor.sh and condor.submit

--- gcloud/condor.sh
+++ gcloud/condor.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-cd /home/starjongmin
-source /home/starjongmin/.bashrc
-conda activate bamcp-negotiation
 cd /home/starjongmin/bucket/BAMCP_negotiation/src
-python selfplay.py --condor_node_idx=$1
+/home/starjongmin/miniconda/envs/bamcp-negotiation/bin/python selfplay.py --condor_node_idx=$1
