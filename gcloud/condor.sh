3024d22dc9c2d115102d0ad7edf0b5c95ba45134
Jongmin Lee <starjongmin@gmail.com>
2019-08-27 15:09:16 +0000

    condor.sh condor.submit

--- gcloud/condor.sh
+++ gcloud/condor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/starjongmin/bucket/BAMCP_negotiation/src
-/home/starjongmin/miniconda/envs/bamcp-negotiation/bin/python selfplay.py --condor_node_idx=$1
+/home/starjongmin/miniconda/envs/bamcp-negotiation/bin/python selfplay.py --condor_node_idx=$1 --condor_num_nodes=100
